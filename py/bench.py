3b488fa5b8e19770b9c6e8e1cea598fb2b26d409
Douglas Orr <douglaso@graphcore.ai>
2026-01-14 02:55:34 +0000

    Refactoring and meta-parameter sweeping

--- py/bench.py
+++ py/bench.py
@@ -1,8 +1,6 @@
 """Triton implementations and benchmarks."""
 
-import sys
 from dataclasses import dataclass
-from functools import partial
 from typing import Callable
 
 import torch
@@ -25,6 +23,10 @@ def measure_time(fn: Callable[[None], None], reps: int) -> float:
         with torch.cuda.graph(g):
             for i in range(reps):
                 fn(i)
+    torch.cuda.synchronize()
+
+    # One final warmup replay
+    g.replay()
     torch.cuda.synchronize()
 
     # Measure time
@@ -58,8 +60,33 @@ class Settings:
     k: int
     n: int
     g: int
+    bits: int
     copies: int
     reps: int
+
+
+@dataclass
+class Result:
+    name: str
+    bytes_rw: int
+    ops: int
+    time_s: float
+
+    def __str__(self) -> str:
+        bandwidth = self.bytes_rw / self.time_s / 1e9
+        ops_per_s = self.ops / self.time_s / 1e12
+        return "  ".join(
+            [
+                f"{self.name:<25}",
+                f"{self.time_s*1e6:>6.1f} us",
+                f"{bandwidth:>6.1f} GB/s",
+                f"{ops_per_s:>6.1f} TFLOPS",
+            ]
+        )
+
+
+class UnsupportedSettings(Exception):
+    pass
 
 
 class Benchmark:
@@ -77,15 +104,15 @@ class Benchmark:
     def setup(self, s: Settings) -> Callable[[], None]:
         raise NotImplementedError
 
-    def run(self, s: Settings) -> None:
+    def run(self, s: Settings) -> Result:
         torch.manual_seed(42)
         fn = self.setup(s)
         time_s = measure_time(fn, s.reps)
-        bandwidth = self.bytes_rw(s) / time_s / 1e9
-        ops_per_s = self.ops(s) / time_s / 1e12
-        print(
-            f"{self.name:<25}  {time_s*1e6:>6.1f} us  {bandwidth:>6.1f} GB/s  {ops_per_s:>6.1f} TFLOPS",
-            file=sys.stderr,
+        return Result(
+            name=self.name,
+            bytes_rw=self.bytes_rw(s),
+            ops=self.ops(s),
+            time_s=time_s,
         )
 
 
@@ -110,6 +137,8 @@ class RegisterBenchmark_mv(Benchmark):
         return 2 * s.n * s.k
 
     def setup(self, s: Settings) -> Callable[[], None]:
+        if s.m != 1 or s.bits != 16:
+            raise UnsupportedSettings("RegisterBenchmark_mv only supports m=1, bits=16")
         a = torch.randn((s.copies, s.k), device="cuda", dtype=self.dtype)
         b = torch.randn((s.copies, s.n, s.k), device="cuda", dtype=self.dtype)
         out = torch.empty((s.copies, s.n), device="cuda", dtype=self.dtype)
@@ -123,18 +152,15 @@ class RegisterBenchmark_mv_lut8(Benchmark):
         self,
         name: str,
         fn: Callable[[Tensor, Tensor, Tensor, Tensor, Tensor], None],
-        bits: int,
         dtype: torch.dtype = torch.bfloat16,
     ):
-        assert 8 % bits == 0, "bits must divide 8"
         super().__init__(name)
         self.fn = fn
-        self.bits = bits
         self.dtype = dtype
 
     def bytes_rw(self, s: Settings) -> int:
         # Don't count the LUT, as we don't use `copies` for that
-        b_size = s.n * (s.k * self.bits) // 8
+        b_size = s.n * (s.k * s.bits) // 8
         a_bs_out_size = (s.k + s.n * (s.k // s.g) + s.n) * self.dtype.itemsize
         return a_bs_out_size + b_size
 
@@ -142,18 +168,22 @@ class RegisterBenchmark_mv_lut8(Benchmark):
         return 2 * s.n * s.k
 
     def setup(self, s: Settings) -> Callable[[], None]:
+        if s.m != 1 or s.bits not in [1, 2, 4, 8]:
+            raise UnsupportedSettings(
+                "RegisterBenchmark_mv only supports m=1, bits in [1, 2, 4, 8]"
+            )
         assert s.k % s.g == 0, "k must be divisible by g"
         torch.manual_seed(42)
         a = torch.randn((s.copies, s.k), device="cuda", dtype=torch.bfloat16)
         bq = torch.randint(
             0,
             256,
-            (s.copies, s.n, (s.k * self.bits) // 8),
+            (s.copies, s.n, (s.k * s.bits) // 8),
             device="cuda",
             dtype=torch.uint8,
         )
-        lut = torch.linspace(-1, 1, 2**self.bits, device="cuda", dtype=torch.bfloat16)
-        lut8 = torch.cartesian_prod(*[lut] * (8 // self.bits)).view(256, -1)
+        lut = torch.linspace(-1, 1, 2**s.bits, device="cuda", dtype=torch.bfloat16)
+        lut8 = torch.cartesian_prod(*[lut] * (8 // s.bits)).view(256, -1)
         bs = torch.rand(
             (s.copies, s.n, s.k // s.g), device="cuda", dtype=torch.bfloat16
         )
@@ -181,6 +211,8 @@ class RegisterBenchmark_mm(Benchmark):
         return 2 * s.m * s.k * s.n
 
     def setup(self, s: Settings) -> Callable[[], None]:
+        if s.bits != 16:
+            raise UnsupportedSettings("RegisterBenchmark_mm only supports bits=16")
         a = torch.randn((s.copies, s.m, s.k), device="cuda", dtype=self.dtype)
         b = torch.randn((s.copies, s.n, s.k), device="cuda", dtype=self.dtype)
         out = torch.empty((s.copies, s.m, s.n), device="cuda", dtype=self.dtype)
@@ -194,18 +226,15 @@ class RegisterBenchmark_mm_lut8(Benchmark):
         self,
         name: str,
         fn: Callable[[Tensor, Tensor, Tensor, Tensor, Tensor], None],
-        bits: int,
         dtype: torch.dtype = torch.bfloat16,
     ):
-        assert 8 % bits == 0, "bits must divide 8"
         super().__init__(name)
         self.fn = fn
-        self.bits = bits
         self.dtype = dtype
 
     def bytes_rw(self, s: Settings) -> int:
         # Don't count the LUT, as we don't use `copies` for that
-        b_size = s.n * (s.k * self.bits) // 8
+        b_size = s.n * (s.k * s.bits) // 8
         a_bs_out_size = (
             s.m * s.k + s.n * (s.k // s.g) + s.m * s.n
         ) * self.dtype.itemsize
@@ -215,18 +244,22 @@ class RegisterBenchmark_mm_lut8(Benchmark):
         return 2 * s.m * s.k * s.n
 
     def setup(self, s: Settings) -> Callable[[], None]:
+        if s.bits not in [1, 2, 4, 8]:
+            raise UnsupportedSettings(
+                "RegisterBenchmark_mm only supports bits in [1, 2, 4, 8]"
+            )
         assert s.k % s.g == 0, "k must be divisible by g"
         torch.manual_seed(42)
         a = torch.randn((s.copies, s.m, s.k), device="cuda", dtype=torch.bfloat16)
         bq = torch.randint(
             0,
             256,
-            (s.copies, s.n, (s.k * self.bits) // 8),
+            (s.copies, s.n, (s.k * s.bits) // 8),
             device="cuda",
             dtype=torch.uint8,
         )
-        lut = torch.linspace(-1, 1, 2**self.bits, device="cuda", dtype=torch.bfloat16)
-        lut8 = torch.cartesian_prod(*[lut] * (8 // self.bits)).view(256, -1)
+        lut = torch.linspace(-1, 1, 2**s.bits, device="cuda", dtype=torch.bfloat16)
+        lut8 = torch.cartesian_prod(*[lut] * (8 // s.bits)).view(256, -1)
         bs = torch.rand(
             (s.copies, s.n, s.k // s.g), device="cuda", dtype=torch.bfloat16
         )
@@ -245,27 +278,33 @@ class RegisterBenchmark_mm_lut8(Benchmark):
 # a   :: [k, dtype]
 # b   :: [n, k, dtype]
 # out :: [n, dtype]
+@triton.autotune(
+    configs=[
+        # Hand-tuned for (n, k) = (4096, 4096) on L40S
+        triton.Config(dict(BLOCK_SIZE=2048), num_stages=1, num_warps=16),
+    ],
+    key=["k"],
+)
 @triton.jit
 def kernel__mv(a_ptr, b_ptr, out_ptr, k, BLOCK_SIZE: tl.constexpr) -> None:
-    i_n = tl.program_id(axis=0)
-    acc = 0.0
-    for ik0 in range(0, k, BLOCK_SIZE):
-        ik = ik0 + tl.arange(0, BLOCK_SIZE)
-        a = tl.load(a_ptr + ik)
-        b = tl.load(b_ptr + i_n * k + ik)
-        prod = a.to(tl.float32) * b.to(tl.float32)
-        acc += tl.sum(prod, axis=0)
-    tl.store(out_ptr + i_n, acc)
+    in_ = tl.program_id(axis=0)
+    out = tl.zeros((), dtype=tl.float32)
+    for i in range(0, tl.cdiv(k, BLOCK_SIZE)):
+        ik = i * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        a = tl.load(a_ptr + ik, mask=ik < k, other=0.0, eviction_policy="evict_last")
+        b = tl.load(
+            b_ptr + in_ * k + ik, mask=ik < k, other=0.0, eviction_policy="evict_first"
+        )
+        out += tl.sum(a.to(tl.float32) * b.to(tl.float32), axis=0)
+    tl.store(out_ptr + in_, out)
 
 
 def run_mv(a: Tensor, b: Tensor, out: Tensor) -> None:
-    block_size = min(2048, a.shape[0])
     n, k = b.shape
     assert a.shape == (k,)
     assert out.shape == (n,)
-    assert k % block_size == 0
     grid = (n,)
-    kernel__mv[grid](a, b, out, k, BLOCK_SIZE=block_size)
+    kernel__mv[grid](a, b, out, k)
 
 
 def test_mv() -> None:
@@ -293,11 +332,7 @@ def mm_lut_ref_unscaled(
     return torch.matmul(a, b.T, out=out)
 
 
-RegisterBenchmark_mv_lut8("mv_8b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=8)
-RegisterBenchmark_mv_lut8("mv_4b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=4)
-RegisterBenchmark_mv_lut8("mv_2b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=2)
-RegisterBenchmark_mv_lut8("mv_1b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=1)
-
+RegisterBenchmark_mv_lut8("mv_lut8_ref_unscaled", mm_lut_ref_unscaled)
 
 ### mm_lut_ref
 
@@ -311,10 +346,7 @@ def mm_lut_ref(
     return torch.matmul(a, b.T, out=out)
 
 
-RegisterBenchmark_mv_lut8("mv_8b_lut8_ref", mm_lut_ref, bits=8)
-RegisterBenchmark_mv_lut8("mv_4b_lut8_ref", mm_lut_ref, bits=4)
-RegisterBenchmark_mv_lut8("mv_2b_lut8_ref", mm_lut_ref, bits=2)
-RegisterBenchmark_mv_lut8("mv_1b_lut8_ref", mm_lut_ref, bits=1)
+RegisterBenchmark_mv_lut8("mv_lut8_ref", mm_lut_ref)
 
 
 ### mv_lut8
@@ -325,6 +357,15 @@ RegisterBenchmark_mv_lut8("mv_1b_lut8_ref", mm_lut_ref, bits=1)
 # lut :: [256, E, dtype]
 # bs  :: [n, k/G, dtype]
 # out :: [n, dtype]
+@triton.autotune(
+    configs=[
+        # Hand-tuned for 1-8b, n = k in 3-8k on L40S
+        triton.Config(dict(BLOCK_SIZE=256), num_stages=1, num_warps=1),
+        triton.Config(dict(BLOCK_SIZE=512), num_stages=1, num_warps=1),
+        triton.Config(dict(BLOCK_SIZE=1024), num_stages=1, num_warps=1),
+    ],
+    key=["k", "ELEMENTS_PER_BYTE", "GROUP_SIZE"],
+)
 @triton.jit
 def kernel__mv_lut8(
     a_ptr,
@@ -339,29 +380,51 @@ def kernel__mv_lut8(
     BLOCK_SIZE: tl.constexpr,
 ) -> None:
     BLOCK_ELEMENTS: tl.constexpr = BLOCK_SIZE * ELEMENTS_PER_BYTE
+    tl.static_assert(BLOCK_ELEMENTS % GROUP_SIZE == 0)
     BLOCK_GROUPS: tl.constexpr = BLOCK_ELEMENTS // GROUP_SIZE
     n = tl.program_id(axis=0)
 
-    ai_ptr = a_ptr + tl.arange(0, BLOCK_ELEMENTS)
-    bi_ptr = b_ptr + n * (k // ELEMENTS_PER_BYTE) + tl.arange(0, BLOCK_SIZE)
-    bsi_ptr = bs_ptr + n * (k // GROUP_SIZE) + tl.arange(0, BLOCK_GROUPS)
+    offs_a = tl.arange(0, BLOCK_ELEMENTS)
+    offs_b = tl.arange(0, BLOCK_SIZE)
+    offs_bs = tl.arange(0, BLOCK_GROUPS)
+    a_ptrs = a_ptr + offs_a
+    b_ptrs = b_ptr + n * (k // ELEMENTS_PER_BYTE) + offs_b
+    bs_ptrs = bs_ptr + n * (k // GROUP_SIZE) + offs_bs
     out = tl.zeros((), dtype=tl.float32)
-    for _ in range(0, k // BLOCK_ELEMENTS):
-        a = tl.load(ai_ptr)
-        bq = tl.load(bi_ptr)
+
+    for ik in range(0, tl.cdiv(k, BLOCK_ELEMENTS)):
+        a = tl.load(
+            a_ptrs,
+            mask=offs_a + ik * BLOCK_ELEMENTS < k,
+            other=0.0,
+            eviction_policy="evict_last",
+        )
+        bq = tl.load(
+            b_ptrs,
+            mask=offs_b + ik * BLOCK_SIZE < (k // ELEMENTS_PER_BYTE),
+            other=0,
+            eviction_policy="evict_first",
+        )
+        # Note: since bq is always in [0, 255], no need to mask `bu` loads.
         bu = tl.load(
             lut_ptr
             + ELEMENTS_PER_BYTE * tl.cast(bq[:, None], tl.int32)
-            + tl.arange(0, ELEMENTS_PER_BYTE)
+            + tl.arange(0, ELEMENTS_PER_BYTE),
+            eviction_policy="evict_last",
         )
-        bs = tl.load(bsi_ptr)
+        bs = tl.load(
+            bs_ptrs,
+            mask=offs_bs + ik * BLOCK_GROUPS < (k // GROUP_SIZE),
+            other=0.0,
+            eviction_policy="evict_first",
+        )
         b = tl.reshape(
             tl.reshape(bu, [BLOCK_GROUPS, GROUP_SIZE]) * bs[:, None], BLOCK_ELEMENTS
         )
         out += tl.sum(a.to(tl.float32) * b.to(tl.float32))
-        ai_ptr += BLOCK_ELEMENTS
-        bi_ptr += BLOCK_SIZE
-        bsi_ptr += BLOCK_GROUPS
+        a_ptrs += BLOCK_ELEMENTS
+        b_ptrs += BLOCK_SIZE
+        bs_ptrs += BLOCK_GROUPS
 
     tl.store(out_ptr + n, out)
 
@@ -379,10 +442,6 @@ def run_mv_lut8(a: Tensor, b: Tensor, lut: Tensor, bs: Tensor, out: Tensor) -> N
     assert bs.shape == (n, k // group_size)
     assert lut.shape == (256, elements_per_byte)
     assert all([t.is_contiguous() for t in [a, b, lut, bs, out]])
-    # heuristic
-    block_size = min(2048, k // elements_per_byte)
-    assert k % (elements_per_byte * block_size) == 0
-    assert block_size % group_size == 0
     # kernel
     grid = (n,)
     kernel__mv_lut8[grid](
@@ -394,7 +453,6 @@ def run_mv_lut8(a: Tensor, b: Tensor, lut: Tensor, bs: Tensor, out: Tensor) -> N
         k,
         ELEMENTS_PER_BYTE=elements_per_byte,
         GROUP_SIZE=group_size,
-        BLOCK_SIZE=block_size,
     )
 
 
@@ -412,10 +470,7 @@ def test_mv_4b_lut8() -> None:
     assert_rmsen(mm_lut_ref(a, bq, lut8, bs), out, tol=1e-2)
 
 
-RegisterBenchmark_mv_lut8("mv_8b_lut8", run_mv_lut8, bits=8)
-RegisterBenchmark_mv_lut8("mv_4b_lut8", run_mv_lut8, bits=4)
-RegisterBenchmark_mv_lut8("mv_2b_lut8", run_mv_lut8, bits=2)
-RegisterBenchmark_mv_lut8("mv_1b_lut8", run_mv_lut8, bits=1)
+RegisterBenchmark_mv_lut8("mv_lut8", run_mv_lut8)
 
 ### mm
 
@@ -458,7 +513,7 @@ def _autotune_configs() -> list[triton.Config]:
 # a :: [m, k, dtype]
 # b :: [n, k, dtype]
 # out :: [m, n, dtype]
-@triton.autotune(configs=_autotune_configs(), key=["m", "n", "k", "DTYPE"])
+@triton.autotune(configs=_autotune_configs(), key=["m", "n", "k"])
 @triton.jit
 def kernel__mm(
     a_ptr,
@@ -508,8 +563,18 @@ def kernel__mm(
     # Compute a block of `out`, iterating over the k dimension.
     out = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
     for ik in range(0, tl.cdiv(k, BLOCK_SIZE_K)):
-        a = tl.load(a_ptrs, mask=offs_k[None, :] < k - ik * BLOCK_SIZE_K, other=0.0)
-        b = tl.load(b_ptrs, mask=offs_k[None, :] < k - ik * BLOCK_SIZE_K, other=0.0)
+        a = tl.load(
+            a_ptrs,
+            mask=offs_k[None, :] < k - ik * BLOCK_SIZE_K,
+            other=0.0,
+            eviction_policy="evict_last",
+        )
+        b = tl.load(
+            b_ptrs,
+            mask=offs_k[None, :] < k - ik * BLOCK_SIZE_K,
+            other=0.0,
+            eviction_policy="evict_first",
+        )
         out = tl.dot(a, tl.trans(b), out)
         a_ptrs += BLOCK_SIZE_K
         b_ptrs += BLOCK_SIZE_K
@@ -543,19 +608,8 @@ def test_mm() -> None:
 
 RegisterBenchmark_mm("mm_ref", lambda a, b, out: torch.matmul(a, b.T, out=out))
 RegisterBenchmark_mm("mm", run_mm)
-
-
-### mm_lut_ref_unscaled, mm_lut_ref (mm benchmarks)
-
-RegisterBenchmark_mm_lut8("mm_8b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=8)
-RegisterBenchmark_mm_lut8("mm_4b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=4)
-RegisterBenchmark_mm_lut8("mm_2b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=2)
-RegisterBenchmark_mm_lut8("mm_1b_lut8_ref_unscaled", mm_lut_ref_unscaled, bits=1)
-
-RegisterBenchmark_mm_lut8("mm_8b_lut8_ref", mm_lut_ref, bits=8)
-RegisterBenchmark_mm_lut8("mm_4b_lut8_ref", mm_lut_ref, bits=4)
-RegisterBenchmark_mm_lut8("mm_2b_lut8_ref", mm_lut_ref, bits=2)
-RegisterBenchmark_mm_lut8("mm_1b_lut8_ref", mm_lut_ref, bits=1)
+RegisterBenchmark_mm_lut8("mm_lut8_ref_unscaled", mm_lut_ref_unscaled)
+RegisterBenchmark_mm_lut8("mm_lut8_ref", mm_lut_ref)
 
 
 ### mm_lut8
@@ -567,27 +621,21 @@ RegisterBenchmark_mm_lut8("mm_1b_lut8_ref", mm_lut_ref, bits=1)
 # out :: [m, n, dtype]
 @triton.autotune(
     configs=[
-        # Hand-tuned for 4b, (m, n, k) = (16, 4096, 4096) on L40S
+        # Hand-tuned for 1-8b, m in [16, 64, 256], n = k in 3-8k on L40S
         triton.Config(
-            {
-                "BLOCK_SIZE_M": 16,
-                "BLOCK_SIZE_N": 16,
-                "BLOCK_SIZE_K": 256,
-                "GROUP_SIZE_M": 8,
-            },
+            dict(BLOCK_SIZE_M=16, BLOCK_SIZE_N=16, BLOCK_SIZE_K=256, GROUP_SIZE_M=8),
             num_stages=1,
             num_warps=4,
         ),
-        # Hand-tuned for 4b, (m, n, k) = (256, 4096, 4096) on L40S
         triton.Config(
-            {
-                "BLOCK_SIZE_M": 32,
-                "BLOCK_SIZE_N": 32,
-                "BLOCK_SIZE_K": 64,
-                "GROUP_SIZE_M": 8,
-            },
+            dict(BLOCK_SIZE_M=16, BLOCK_SIZE_N=32, BLOCK_SIZE_K=64, GROUP_SIZE_M=8),
             num_stages=1,
-            num_warps=1,
+            num_warps=2,
+        ),
+        triton.Config(
+            dict(BLOCK_SIZE_M=32, BLOCK_SIZE_N=16, BLOCK_SIZE_K=256, GROUP_SIZE_M=8),
+            num_stages=1,
+            num_warps=2,
         ),
     ],
     key=["m", "n", "k", "ELEMENTS_PER_BYTE", "DTYPE"],
@@ -731,45 +779,61 @@ def test_mm_4b_lut8() -> None:
     assert_rmsen(mm_lut_ref(a, b, lut8, bs), out, tol=1e-2)  # tol too high?
 
 
-RegisterBenchmark_mm_lut8("mm_8b_lut8", run_mm_lut, bits=8)
-RegisterBenchmark_mm_lut8("mm_4b_lut8", run_mm_lut, bits=4)
-RegisterBenchmark_mm_lut8("mm_2b_lut8", run_mm_lut, bits=2)
-RegisterBenchmark_mm_lut8("mm_1b_lut8", run_mm_lut, bits=1)
+RegisterBenchmark_mm_lut8("mm_lut8", run_mm_lut)
 
 
 # Top-level
 
 
-def main() -> None:
+def run_tests() -> None:
+    for name, fn in list(globals().items()):
+        if callable(fn) and name.startswith("test_"):
+            fn()
+
+
+def run_benchmarks(settings: Settings, only: str | None = None) -> None:
+    for name, benchmark in BENCHMARKS.items():
+        if only is None or only == name:
+            try:
+                print(benchmark.run(settings))
+            except UnsupportedSettings as e:
+                pass
+
+
+def run_main() -> None:
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "profile", nargs="?", help="Select a specific method to profile"
     )
-    parser.add_argument("-m", default=16, type=int, help="Dimension m (`mm` only)")
+    parser.add_argument("-m", default=16, type=int, help="Dimension m")
     parser.add_argument("-k", default=4096, type=int, help="Dimension k")
     parser.add_argument("-n", default=4096, type=int, help="Dimension n")
     parser.add_argument("-g", default=64, type=int, help="Dimension g (group size)")
+    parser.add_argument(
+        "-b", "--bits", default=16, type=int, help="Bits per element (1, 2, 4, 8, 16)"
+    )
     parser.add_argument("--copies", default=100, type=int, help="Number of arg copies")
     parser.add_argument("--reps", default=1000, type=int, help="Number of reps")
     args = parser.parse_args()
 
-    # Run test_*
-    top_vars = list(globals().items())
     if args.profile is None:
-        for name, fn in top_vars:
-            if callable(fn) and name.startswith("test_"):
-                fn()
+        run_tests()
 
-    # Run benchmarks
-    settings = Settings(
-        m=args.m, k=args.k, n=args.n, g=args.g, copies=args.copies, reps=args.reps
+    run_benchmarks(
+        Settings(
+            m=args.m,
+            k=args.k,
+            n=args.n,
+            g=args.g,
+            bits=args.bits,
+            copies=args.copies,
+            reps=args.reps,
+        ),
+        only=args.profile,
     )
-    for name, benchmark in BENCHMARKS.items():
-        if args.profile is None or args.profile == name:
-            benchmark.run(settings)
 
 
 if __name__ == "__main__":
-    main()
+    run_main()
